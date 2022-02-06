007a5d6d6cd002e4927cab2f0c98a2a2f8a7d861
Starsky <starsky.m@live.com>
2022-02-06 15:43:55 +0100

    add extension dataSource

--- WebsiteCollection/ViewController.swift
+++ WebsiteCollection/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let urls = ["https://apple.de", "https://hamburg.de", "https://google.de" ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return urls.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = urls[indexPath.row]
+        return cell
+    }
+    
+}
