af5d75f5f7d8c896e3f05dfd476357953289ff70
Starsky <starsky.m@live.com>
2022-02-06 15:05:33 +0100

    Initial Commit

---
+++ WebsiteCollection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebsiteCollection
+//
+//  Created by star on 06.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
